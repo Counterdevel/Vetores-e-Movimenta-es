11c4d39446ce0f086e95f6144bf284eb0041534f
Pierre Muller <pierreeitel@hotmail.com>
2021-03-08 10:57:52 -0300

    Vetores e Perseguição
    Objeto a e Objeto B perseguem Objeto C enquanto ele se move

---
+++ Vetores e Movimentações/Assets/Scripts/Continuo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Continuo : MonoBehaviour
+{
+    public List<Vector3> points;
+    NavMeshAgent agent;
+
+    int index = 0;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(points[index]);
+        index++;
+
+        if (index >= points.Count)
+        {
+            index = 0;
+        }
+    }
+}
---
+++ Vetores e Movimentações/Assets/Scripts/Perseguicao.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Perseguicao : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject perseguido;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.destination = perseguido.transform.position;
+    }
+}
