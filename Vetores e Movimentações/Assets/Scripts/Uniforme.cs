83aa1e6c468840422d720e9ded18bf817a0a20bc
Pierre Muller <pierreeitel@hotmail.com>
2021-03-08 10:24:58 -0300

    Vetores e Movimentações Uniforme
    Objeto se move de Ponto A ao Ponto B

---
+++ Vetores e Movimentações/Assets/Scripts/Uniforme.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Uniforme : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = new Vector3(0, 1, 24);
+    }
+}
